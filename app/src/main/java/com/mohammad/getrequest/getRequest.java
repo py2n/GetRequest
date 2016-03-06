eaf48b235bf4575c57b90a5d4f7071af56122dbd
py2n <janatiyanm@gmail.com>
2016-03-06 08:05:28 -0800

    update get request

---
+++ app/src/main/java/com/mohammad/getrequest/getRequest.java
@@ -0,0 +1,101 @@
+package com.mohammad.getrequest;
+
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class getRequest extends Activity{
+    boolean flag = true;
+    TextView output;
+    ProgressBar pb;
+    List<MyTask> tasks;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+//        getActionBar().setDisplayHomeAsUpEnabled(true);
+        pb = (ProgressBar) findViewById(R.id.progressBar2);
+        output = (TextView) findViewById(R.id.textView);
+        tasks = new ArrayList<>();
+        Bundle data=getIntent().getExtras();
+        String request;
+        request = data.getString("request");
+        requestData(request);
+        pb.setVisibility(View.INVISIBLE);
+    }
+
+
+    protected void updateDisplay(String result) {
+        try {
+            if (result.contains("Congratulation"))
+                output.setText("Congratulation new mac address added to database" + "\n");
+            else if (result.contains("been") && flag == true)
+                output.setText("درخواست روشن شدن با موفقیت ثبت شد \n");
+            else if (result.contains("been") && flag == false)
+                output.setText("درخواست خاموش شدن با موفقیت ثبت شد \n");
+            else if (result.contains("error"))
+                output.setText("در حال حاضر امکان برقراری ارتباط با\n مرکز سرویس دهی موجود نمی باشد \n");
+            if (result.contains("new version"))
+                output.setText("لطفا نرم افزار خود را بروز رسانی کنید");
+
+            output.setText(result);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+    protected void requestData(final String req) {
+        flag = true;
+
+                pb.setVisibility(View.INVISIBLE);
+                RequestPackage p = new RequestPackage();
+                p.setMethod("POST");
+                p.setUri("http://192.168.56.1:8000/rtc");
+                p.setParam("mac_addr", "00:00:00:00:00:45");
+                flag = true;
+                MyTask task = new MyTask();
+                task.execute(p);
+            }
+
+
+    private class MyTask extends AsyncTask<RequestPackage, String, String> {
+        @Override
+        protected void onPreExecute() {
+            if (tasks.size() == 0) {
+                pb.setVisibility(View.VISIBLE);
+            }
+            tasks.add(this);
+        }
+        @Override
+        protected String doInBackground(RequestPackage... params) {
+            String content = HttpManager.getData(params[0]);
+            return content;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            try {
+                tasks.remove(this);
+                if (tasks.size() == 0) {
+                    pb.setVisibility(View.INVISIBLE);
+                }
+                if (result.contains("error"))
+                    updateDisplay("problem on connecting to service provider");
+                updateDisplay(result);
+            } catch (Exception e) {
+                System.out.println("error occured in " + e.toString());
+                updateDisplay("problem on sending the request");
+            }
+        }
+    }
+}
